3aa616b35a54c3ce7feb304e9b49c7fbefddef6f
Dmitrij Dreiling <d.dimka89@gmail.com>
2023-02-02 23:41:00 +0100

    Clean up settings

--- svpb/settings.py
+++ svpb/settings.py
@@ -1,36 +1,41 @@
 # Django settings for svpb project.
 
-import os
-## let's find the root from where the server runs: 
-APPLICATION_DIR = os.path.dirname( globals()[ '__file__' ] )
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '26w5_t=fcjff6vk9$ee(03xa&+1c($ot1ixg)p-f(%v#ad$dqy'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = [
+    ".svpb.de",
+    "127.0.0.1",
+    ".h00227.host-up.de"
+    ]
 
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-LOGIN_URL = "/login/"
 
 OFFLINE = False
 JAHRESENDE = False
 
-
-# DEBUG = False
-DEBUG = True
-
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
+ADMINS = [
     # ('Your Name', 'your_email@example.com'),
-)
+]
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        # Use postgresql in production
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # Use file-based db for local testing and debugging
         'ENGINE': 'django.db.backends.sqlite3',
-        # Database name (for sqlite3 this is the file name)
-        'NAME': 'svpb.sq',
+        'NAME': BASE_DIR / 'svpb.sq',
         # The following settings are not used with sqlite3:
         'USER': 'UserPlaceholder',
         'PASSWORD': 'PasswordPlaceholder',
@@ -39,167 +44,18 @@ DATABASES = {
     }
 }
 
-# print(f"DATABASE: {DATABASES['default']['ENGINE']}")
-    
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [".svpb.de", "127.0.0.1", ".h00227.host-up.de"]
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'Europe/Berlin'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'de'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join (APPLICATION_DIR, '..', 'media')
-
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = 'media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/svpb/svpb/static'
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    # os.path.join (APPLICATION_DIR, '..', 'arbeitsplan', 'static'),
-    os.path.join (APPLICATION_DIR, '..', 'boote', 'static'),
-    os.path.join (APPLICATION_DIR, '..', 'templates'),
-)
-
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '26w5_t=fcjff6vk9$ee(03xa&+1c($ot1ixg)p-f(%v#ad$dqy'
-
-# List of callables that know how to import templates from various sources.
-# TEMPLATE_LOADERS = (
-#    'django.template.loaders.filesystem.Loader',
-#    'django.template.loaders.app_directories.Loader',
-#    'django.template.loaders.eggs.Loader',
-#)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth', 
-    'django.core.context_processors.request',
-    'django.contrib.messages.context_processors.messages',
-    'svpb.context_processors.global_settings', 
-    ) 
-
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join (APPLICATION_DIR, '../templates'), 
-    os.path.join (APPLICATION_DIR, 'templates'),
-    os.path.join (APPLICATION_DIR, '../arbeitsplan/templates'),
-    os.path.join (APPLICATION_DIR, '../mitglieder/templates'),
-    os.path.join (APPLICATION_DIR, '../boote/templates'),
-)
-
-# New TEMPLATES as of Django 1.8, compare https://docs.djangoproject.com/en/1.8/ref/templates/upgrading/
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': TEMPLATE_DIRS, 
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'debug' : True,
-            'context_processors': [
-                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
-                # list if you haven't customized them:
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-            'django.template.context_processors.tz',
-            'django.template.context_processors.request',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]    
-    
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'impersonate.middleware.ImpersonateMiddleware',
-)
-
-# as of Django 1.8: https://stackoverflow.com/questions/56923576/django-e-408-e-409-and-e-410-errors-on-runserver
-MIDDLEWARE = MIDDLEWARE_CLASSES
-
-    
-IMPERSONATE_REDIRECT_URL = "/"
-
-def user_is_vorstand(request):
-    return request.user.groups.filter(name="Vorstand")
-IMPERSONATE_CUSTOM_ALLOW = "svpb.settings.user_is_vorstand"
-
-
-ROOT_URLCONF = 'svpb.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'svpb.wsgi.application'
-
-INSTALLED_APPS = (
+INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django_extensions',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    ## 'django_admin_bootstrapped.bootstrap3',
-    ## 'django_admin_bootstrapped',
-    'django.contrib.admin',
-    'django_extensions',
+    # 'django_admin_bootstrapped.bootstrap3',
+    # 'django_admin_bootstrapped',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'django_tables2',
@@ -211,10 +67,53 @@ INSTALLED_APPS = (
     'sendfile',
     'password_reset',
     'django_select2',
-)
-
+]
 CRISPY_TEMPLATE_PACK = "bootstrap3"
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'impersonate.middleware.ImpersonateMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
+ROOT_URLCONF = 'svpb.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR / 'templates',
+            BASE_DIR / 'svpb/templates',
+            BASE_DIR / 'arbeitsplan/templates',
+            BASE_DIR / 'mitglieder/templates',
+            BASE_DIR / 'boote/templates',
+            ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'debug': True,
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.template.context_processors.request',
+                'django.contrib.messages.context_processors.messages',
+                # 'svpb.context_processors.global_settings'
+            ],
+        },
+    },
+]
+
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'svpb.wsgi.application'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
@@ -245,34 +144,64 @@ LOGGING = {
     }
 }
 
-#####################
-# Own settings:
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+TIME_ZONE = 'Europe/Berlin'
+LANGUAGE_CODE = 'de'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+PHONENUMBER_DEFAULT_REGION = "DE"
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = '/media/'
+LOGIN_URL = "/login/"
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_URL = '/static/'
+SENDFILE_URL = '/media/doc'
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
+MEDIA_ROOT = BASE_DIR / 'media'
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = BASE_DIR / 'svpb/static'
+SENDFILE_ROOT = STATIC_ROOT / 'media/doc'
+# Additional locations of static files
+STATICFILES_DIRS = [
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    # os.path.join (APPLICATION_DIR, '..', 'arbeitsplan', 'static'),
+    BASE_DIR / 'boote/static',
+    BASE_DIR / 'templates'
+    ]
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
-from django.contrib.messages import constants as message_constants
-MESSAGE_TAGS = {message_constants.DEBUG: 'debug',
-                message_constants.INFO: 'info',
-                message_constants.SUCCESS: 'success',
-                message_constants.WARNING: 'warning',
-                message_constants.ERROR: 'danger',}
-
-## Migration to Phyton3
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
-#####
 # XSendfilte interface
 # this will only work with nginx, not in development setup - but that's not too important to test there
-
-SENDFILE_BACKEND = "sendfile.backends.nginx"
-SENDFILE_ROOT = os.path.join(STATIC_ROOT, "media/doc")
-SENDFILE_URL = "/media/doc"
-
+SENDFILE_BACKEND = 'sendfile.backends.nginx'
 
 JAHRESSTUNDEN = 12
 
-# for select2: 
+# for select2:
 SELECT2_BOOTSTRAP = False
 AUTO_RENDER_SELECT2_STATICS = False
 
+IMPERSONATE_REDIRECT_URL = "/"
+IMPERSONATE_CUSTOM_ALLOW = "svpb.settings.user_is_vorstand"
 
-# for phonenumbers:
-PHONENUMBER_DEFAULT_REGION = "DE"
+
+def user_is_vorstand(request):
+    return request.user.groups.filter(name="Vorstand")
