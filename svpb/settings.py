1e2270c9a35de318a14dee6f6042b8237a5d4782
Dmitrij Dreiling <d.dimka89@gmail.com>
2023-02-03 21:15:43 +0100

    Merge email settings to settings

--- svpb/settings.py
+++ svpb/settings.py
@@ -202,6 +202,23 @@ AUTO_RENDER_SELECT2_STATICS = False
 IMPERSONATE_REDIRECT_URL = "/"
 IMPERSONATE_CUSTOM_ALLOW = "svpb.settings.user_is_vorstand"
 
+# email settings:
+EMAIL_HOST = ''
+EMAIL_PORT = 465
+
+DEFAULT_FROM_EMAIL = "test@test.com"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = "XXX"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+# Use console mail backend for local testing and debugging
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Use the standard django SMTP backend for production
+# Note that in addition to the standard django backend, post_office is used for
+# mass mailings (is explicitly imported where needed).
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 def user_is_vorstand(request):
     return request.user.groups.filter(name="Vorstand")
