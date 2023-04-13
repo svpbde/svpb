de02e3ea8efff2faeb6e8de70c1537e7dbcdfef3
Tim Hetkämper <transistortim@hetkaemper.it>
2023-04-13 22:27:02 +0200

    Adapt script for postgres

--- scripts/load_fixtures.sh
+++ scripts/load_fixtures.sh
@@ -1,5 +1,9 @@
-cd .. 
-rm svpb.sq 
+cd ..
+# Delete and recreate database
+sudo -u postgres dropdb --force --interactive svpbdata
+sudo -u postgres createdb svpbdata
+sudo -u postgres psql -c "grant all privileges on database svpbdata to svpbdb;"
+# Init database with fixtures
 python manage.py migrate
 python manage.py loaddata mitglieder/fixtures/*.json
 python manage.py loaddata arbeitsplan/fixtures/*.json
