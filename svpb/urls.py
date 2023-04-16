dee0882f6d69e129f2a70d4e4039e15465294b0d
Tim Hetkämper <transistortim@hetkaemper.it>
2023-04-16 20:19:09 +0200

    Fix whitespace

--- svpb/urls.py
+++ svpb/urls.py
@@ -6,8 +6,9 @@ from django.views.generic import TemplateView
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 
-import arbeitsplan.views, svpb.views
+import arbeitsplan.views
 import mitglieder.views
+import svpb.views
 
 from .activeTest import active_and_login_required
 
@@ -15,22 +16,22 @@ from .activeTest import active_and_login_required
 admin.autodiscover()
 
 
-urlpatterns = [ # '',
+urlpatterns = [
 
-    url (r'^$',
-         TemplateView.as_view (template_name="main.html"),
-         name="main",
-         ),
+    url(r'^$',
+        TemplateView.as_view(template_name="main.html"),
+        name="main",
+        ),
 
-    url (r'^home/$',
-         TemplateView.as_view (template_name="main.html"),
-         name="mainHome",
-         ),
+    url(r'^home/$',
+        TemplateView.as_view(template_name="main.html"),
+        name="mainHome",
+        ),
 
-    url (r'^keinVorstand/$',
-         TemplateView.as_view (template_name='keinVorstand.html'),
-         name="keinVorstand", 
-         ),
+    url(r'^keinVorstand/$',
+        TemplateView.as_view(template_name='keinVorstand.html'),
+        name="keinVorstand",
+        ),
 
     url(r'^arbeitsplan/', include('arbeitsplan.urls')),
 
@@ -38,39 +39,40 @@ urlpatterns = [ # '',
 
     url(r'^boote/', include('boote.urls')),
 
-    url (r'^bootstrap$',
-         TemplateView.as_view(template_name="bootstrap.html"),
-         name="bootstrap", 
-         ),
+    url(r'^bootstrap$',
+        TemplateView.as_view(template_name="bootstrap.html"),
+        name="bootstrap",
+        ),
 
     url(r'^about$',
-        TemplateView.as_view (template_name="about.html"),
-        name="about", 
+        TemplateView.as_view(template_name="about.html"),
+        name="about",
         ),
 
     url(r'^dsgvo$',
-        TemplateView.as_view (template_name="datenschutzerklaerung.html"),
-        name="about", 
+        TemplateView.as_view(template_name="datenschutzerklaerung.html"),
+        name="about",
         ),
 
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/', admin.site.urls),
 
-    url (r'^login/',
-         svpb.views.SvpbLogin.as_view(),
-         name="login"),
+    url(r'^login/',
+        svpb.views.SvpbLogin.as_view(),
+        name="login"),
 
-    url (r'^logout/', svpb.views.logout_view),
+    url(r'^logout/', svpb.views.logout_view),
 
-    url (r'^password/change/$',
-         active_and_login_required(mitglieder.views.PasswordChange.as_view()),
-         name='password_change',
-         ),
+    url(r'^password/change/$',
+        active_and_login_required(mitglieder.views.PasswordChange.as_view()),
+        name='password_change',
+        ),
 
     # media for manual intergration:
     url(r'^manual/',
         active_and_login_required(arbeitsplan.views.MediaChecks.as_view()),
-        name="MediaCheck"),
+        name="MediaCheck",
+        ),
 
     # Impersonation of other users:
     url(r'^impersonate/liste/$',
@@ -79,13 +81,12 @@ urlpatterns = [ # '',
         ),
     url(r'^impersonate/', include('impersonate.urls')),
 
-                       # password reset; compare http://django-password-reset.readthedocs.org/en/latest/quickstart.html
+    # password reset; compare http://django-password-reset.readthedocs.org/en/latest/quickstart.html
     url(r'^reset/', include('password_reset.urls')),
 
-                       # django select2, see: https://github.com/applegrew/django-select2
+    # django select2, see: https://github.com/applegrew/django-select2
     url(r'^select2/', include('django_select2.urls')),
 
-    # )
     ]
 
 if settings.DEBUG:
@@ -97,6 +98,6 @@ if settings.DEBUG:
     # urlpatterns.append(
     #     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
     #     'document_root': settings.MEDIA_ROOT}))
-    
 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
