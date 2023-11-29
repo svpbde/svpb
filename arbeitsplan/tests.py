4453363de1c2d63832c3fbcfb95735e0b2c0aecf
Tim Hetkämper <transistortim@hetkaemper.it>
2023-11-29 21:16:07 +0100

    Clean up tests

--- arbeitsplan/tests.py
+++ arbeitsplan/tests.py
@@ -1,12 +1,9 @@
-"""Tests of arbeitsplan app
-"""
-from django.contrib.auth.models import User
-from django.core.mail import outbox
+"""Tests of arbeitsplan app"""
+from django.core import mail
 from django.test import Client, TestCase
 
-from arbeitsplan.models import *
+from arbeitsplan.models import Mitglied
 
-from pprint import pprint as pp
 
 class SimpleTest(TestCase):
     fixtures = [
@@ -17,36 +14,11 @@ class SimpleTest(TestCase):
         'mitglieder/fixtures/po.json',
         'mitglieder/fixtures/user.json'
     ]
-
-    # in fixture, all users should have this password:
+    # In fixtures, all users should have this password
     plainpassword = "Test"
     superuser = "Superuser"
 
-    def meldungOnlyOne(self):
-        """
-        Ensure integrity of Meldung:
-        for any user/aufgabe combination, there must
-        be AT MOST one meldung
-        """
-
-        for u in User.objects.all():
-            for a in Aufgabe.objects.all():
-                pass
-        
-    def setUp(self):
-        """
-        Rewrite the passwords for the testusers
-        """
-        # not necessary; fixture has been adapted so
-        # that all users have same password (hopefully!)
-
-        # u = User.objects.get(username="hkarl")
-        # u.set_password(self.plainpassword)
-        # u.save()
-        pass
-
     def login_user(self, user):
-
         cl = Client()
         response = cl.post(
             '/login/',
@@ -54,20 +26,16 @@ class SimpleTest(TestCase):
              'password': self.plainpassword},
             follow=True,
             )
-
-        self.assertTrue(response.status_code == 200)
+        self.assertEqual(response.status_code, 200)
 
         return cl, response
 
     def test_login_works(self):
         cl, response = self.login_user(self.superuser)
+        self.assertNotIn("login", response.request["PATH_INFO"])
 
     def test_leistung_bearbeiten(self):
-        """
-        According to github report #14, lesitung bearbeiten fails.
-        Problem not really clear from the description, however? :-(
-        """
-
+        """Test if leistungenBearbeiten page loads."""
         cl, response = self.login_user(self.superuser)
         response = cl.get('/arbeitsplan/leistungenBearbeiten/z=me/')
         self.assertEqual(response.status_code, 200)
@@ -76,112 +44,21 @@ class SimpleTest(TestCase):
         response = cl.get('/arbeitsplan/leistungenBearbeiten/z=me/?last_name=&first_name=&aufgabengruppe=3&von=&bis=&status=OF&status=RU&filter=Filter+anwenden')
         self.assertEqual(response.status_code, 200)
 
-    def _test_changes_in_stundenplan(self):
-        cl, response = self.login_user(self.superuser)
-        response = cl.get('/arbeitsplan/stundenplaene/6/')
-        # print response
-        self.assertEqual(response.status_code, 200)
-
-
-    def check_stundenplaene_unique(self):
-        problem = False
-
-        for a in Aufgabe.objects.all():
-            s = Stundenplan.objects.filter(aufgabe=a)
-            for u in range(8, 24):
-                s2 = s.filter(uhrzeit=u)
-                c = s2.count()
-                if c>1:
-                    problem = True
-                    ## print a, u, s2.count()
-                    ## print [x.anzahl for x in s2]
-
-        return problem
-
-    def _test_stundenplaene_unique(self):
-        """for all comubinations of Aufgabe and uzhrzeit,
-        there must be at most ONE entry in stundenplaene"""
-
-        problem1 = self.check_stundenplaene_unique()
-
-        # try to kick out all the redundant combiantions:
-        if problem1:
-            for a in Aufgabe.objects.all():
-                for u in range(8, 24):
-                    s = Stundenplan.objects.filter(aufgabe=a).filter(uhrzeit=u)
-                    c = s.count()
-                    if c > 1:
-                        tmp = [x for x in s]
-                        # print c
-                        for d in tmp[:-1]:
-                            # print d
-                            d.delete()
-
-            problem2 = self.check_stundenplaene_unique()
-
-            if problem2:
-                print("Problem persisted after clean up - giving up")
-                self.assertFalse(problem2)
-            else:
-                # dumping the sanitized data
-                from django.core.management import call_command
-                output = open("sanitized.json", "w")
-                call_command('dumpdata', 'arbeitsplan', format='json',
-                             indent=3, stdout=output)
-                output.close()
-
-        self.assertFalse(False)
-
-    def _test_stundenplaene_duplicates(self):
-        """is there any process by which studneplaene entries become duplicates?
-        Make a change to an Aufgabe Stundenplan
-
-        TODO: not clear how to really do a post here with acceptable overhead
-        for writing the test
-        """
-
-        # the database better be ok before we test this:
-        problem = self.check_stundenplaene_unique()
-        self.assertFalse(problem)
-
-        cl, response = self.login_user(self.superuser)
-        response = cl.get('/arbeitsplan/aufgabeEditieren/7/')
-        self.assertEqual(response.status_code, 200)
-
-        pp(response.context, indent=4)
-
-        ## cl.post('/arbeitsplan/aufgabeEditieren/7/',
-        ##         )
-        self.assertTrue(True)
-
-
     def test_zuteilung_email(self):
         """do the zuteilung emails look right?"""
-
-        # make sure there is at least a single user with a zuteilung_noetig true
-
+        # Make sure there is at least a single user with a zuteilung_noetig true
         u = Mitglied.objects.get(mitgliedsnummer="00003")
         u.zuteilungBenachrichtigungNoetig = True
         u.save()
 
         cl, response = self.login_user(self.superuser)
-        self.assertEqual(response.status_code, 200)
 
         response = cl.get("/arbeitsplan/benachrichtigen/zuteilung/",
                           follow=True)
         self.assertEqual(response.status_code, 200)
-        
         # the user's email must appear in the response page:
         self.assertContains(response, u.user.email, html=False)
 
-        # print response.content
-
-        # do we have the dicts available?
-        # print "REQUEST"
-        # print response.request
-        # print "---------------------"
-        # pp (response.context[-1])
-        
         # let's generate the email
         response = cl.post("/arbeitsplan/benachrichtigen/zuteilung/",
                            {'eintragen': 'Benachrichtigungen eintragen',
@@ -190,14 +67,10 @@ class SimpleTest(TestCase):
                             'anmerkung_4': 'anmerkungtest'},
                            follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(outbox), 0)
+        self.assertEqual(len(mail.outbox), 0)
 
         # and actually send it off:
-
-        # TODO: check how to deal with post_office based mai handling
-        #
-        # response = cl.get("/arbeitsplan/benachrichtigen/senden/",
-        #                   follow=True)
-        #
-        # self.assertEqual(len(outbox), 1)
-        # print outbox[0]
+        response = cl.get("/arbeitsplan/benachrichtigen/senden/",
+                          follow=True)
+        self.assertEqual(len(mail.outbox), 1)
+        print(mail.outbox[0])
--- mitglieder/tests.py
+++ mitglieder/tests.py
@@ -1,232 +1,172 @@
-"""Tests of mitglieder administration
-"""
-from django.contrib.auth.models import User
-from django.test import Client, TestCase
+"""Tests of mitglieder administration"""
+import re
 
-from pprint import pprint as pp
+from django.contrib.auth.models import User
+from django.core import mail
+from django.test import Client, TestCase
 
 from mitglieder import views
 
 
 class MitgliederTest(TestCase):
     fixtures = [
-        'arbeitsplan/fixtures/members.json',
-        'arbeitsplan/fixtures/taskgroups.json',
-        'arbeitsplan/fixtures/tasks.json',
-        'mitglieder/fixtures/groups.json',
-        'mitglieder/fixtures/po.json',
-        'mitglieder/fixtures/user.json'
+        "arbeitsplan/fixtures/members.json",
+        "arbeitsplan/fixtures/taskgroups.json",
+        "arbeitsplan/fixtures/tasks.json",
+        "mitglieder/fixtures/groups.json",
+        "mitglieder/fixtures/po.json",
+        "mitglieder/fixtures/user.json",
     ]
+    # In fixtures, all users should have this password
     plainpassword = "Test"
     superuser = "Superuser"
 
     def login_user(self, user, password=None):
         cl = Client()
-
         response = cl.post(
-            '/login/',
-            {'username': user,
-             'password': password or self.plainpassword},
+            "/login/",
+            {"username": user, "password": password or self.plainpassword},
             follow=True,
-            )
-
-        # pp(response)
-        # pp(response.status_code)
-        # pp(response.redirect_chain)
-        # print "----"
-        # print "Content:"
-        # pp(response.content)
-        # print "----"
-        # print "Context:"
-        # pp(len(response.context))
-        # pp(response.context)
-        # print "-----"
-        # print "messages:"
-        # pp([m.message for m in list(response.context['messages'])])
-
-        self.assertTrue(response.status_code == 200)
+        )
+        self.assertEqual(response.status_code, 200)
 
         return cl, response
 
     def test_login_works(self):
         cl, response = self.login_user(self.superuser)
-
-        # print "analysing context:"
-        # pp(response.request)
-        # print "----"
-        # pp(response.context[-1])
-        # pp([m.message for m in list(response.context['messages'])])
-
-        self.assertTrue('login' not in response.request['PATH_INFO'])
+        self.assertNotIn("login", response.request["PATH_INFO"])
 
     def test_wrong_password_fails(self):
         cl, response = self.login_user(self.superuser, "xxx")
-        self.assertFalse('login' not in response.request['PATH_INFO'])
+        self.assertIn("login", response.request["PATH_INFO"])
 
     def test_add_user(self):
-
-        print("adding user")
-
         cl, response = self.login_user(self.superuser)
-
-        # print "adding"
-
-        response = cl.get(
-                '/accounts/add/')
-
-        # print "response after get:"
-        pp(response)
-
+        response = cl.get("/accounts/add/")
         response = cl.post(
-            '/accounts/add/',
-            {'firstname': 'Peter',
-             'lastname': 'Pan',
-             'email': 'peter@pan.com',
-             'mitgliedsnummer': '666666',
-             'geburtsdatum': '01.01.2000',
-             'strasse': 'Testweg',
-             'gender': 'M',
-             'plz': '12345',
-             'ort': 'Teststadt',
-             'status': "Er",
-             'arbeitslast': '12',
-             },
+            "/accounts/add/",
+            {
+                "firstname": "Peter",
+                "lastname": "Pan",
+                "email": "peter@pan.com",
+                "mitgliedsnummer": "666666",
+                "geburtsdatum": "01.01.2000",
+                "strasse": "Testweg",
+                "gender": "M",
+                "plz": "12345",
+                "ort": "Teststadt",
+                "status": "Er",
+                "arbeitslast": "12",
+            },
             follow=True,
         )
 
-        # assert the main conditions:
+        # Assert the main conditions
         self.assertEqual(response.status_code, 200)
-        self.assertRedirects(response,
-                             views.AccountAdd.success_url + '/')
+        self.assertRedirects(response, views.AccountAdd.success_url + "/")
         # TODO: check the success URLs in the various views;
         # they probably should all take a trailing slash
 
-        # get message from context and check that expected text is there
-        message = list(response.context['messages'])[0]
+        # Get message from context and check that expected text is there
+        message = list(response.context["messages"])[0]
         self.assertEqual(message.tags, "success")
-        self.assertTrue("erfolgreich angelegt" in message.message)
-
-        # self.assertFormError(response,)
-        print("response after post:")
-        pp(response)
-        pp(response.status_code)
+        self.assertIn("erfolgreich angelegt", message.message)
 
         # Activate user to allow login
         peter = User.objects.get(username="666666")
         peter.is_active = True
         peter.save()
 
-        # try to get the new password link:
-        # check: does this have a redirect loop?
+        # Try to get the new password link, check for redirect loop
         cl2 = Client()
         response = cl2.post(
-            '/reset/recover/',
-            {'username_or_email': '666666'},
+            "/reset/recover/",
+            {"username_or_email": "666666"},
             follow=False,
         )
-
         self.assertEqual(response.status_code, 302)
-        pp(response.context)
 
-        from django.core.mail import outbox
-        sentmail = outbox[0]
-        # sentmail should be an instance ov django.core.mail.message.EmailMultiAlternatives
+        sentmail = mail.outbox[0]
         body = sentmail.body
 
-        # grab the reset URL out of the mail body.
-        # we exploit the fact that the URL sits on a separate page
-        import re
-        url = re.search('/reset/reset/.*/', body)
+        # Grab the reset URL out of the mail body...
+        url = re.search("/reset/reset/.*/", body)
         url = url.group(0)
 
-        # and sent the new password there:
-        response = cl2.post(url,
-                            {'password1': self.plainpassword,
-                             'password2': self.plainpassword,},
-                            follow=True)
-
+        # ...and send the new password there
+        response = cl2.post(
+            url,
+            {
+                "password1": self.plainpassword,
+                "password2": self.plainpassword,
+            },
+            follow=True,
+        )
         self.assertEqual(response.status_code, 200)
 
-        # and now try to login with the new password
-        cl3, response = self.login_user('666666')
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('login' not in response.request['PATH_INFO'])
+        # Try to login with the new password
+        cl3, response = self.login_user("666666")
+        self.assertNotIn("login", response.request["PATH_INFO"])
 
     def test_profile_incomplete(self):
         cl, response = self.login_user(self.superuser)
-        self.assertEqual(response.status_code, 200)
+        self.assertNotIn("login", response.request["PATH_INFO"])
 
-        message = list(response.context['messages'])[0]
-
+        message = list(response.context["messages"])[0]
         self.assertEqual(message.tags, "warning")
+        self.assertContains(response, "Profilangaben sind unvoll")
 
-        self.assertContains(response,
-                            "Profilangaben sind unvoll")
-
-        # Add a phone number , and then log out and login again
+        # Add a phone number, and then log out and log in again
         response = cl.get("/accounts/edit/")
-
-        form = response.context['form']
-
+        form = response.context["form"]
         data = form.initial
-        data['festnetz'] = "030 12312345"
+        data["festnetz"] = "030 12312345"
         # Note: all fields must have values, else validation
         # throws up errors and the form is not redirected
-        data['mobil'] = ""
+        data["mobil"] = ""
 
-        response = cl.post('/accounts/edit/',
-                            data,
-                           follow=True)
+        response = cl.post("/accounts/edit/", data, follow=True)
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual("/", response.request['PATH_INFO'])
+        self.assertEqual("/", response.request["PATH_INFO"])
 
-        # Note: the use of lambda, to make sure the expression
+        # Note the use of lambda, to make sure the expression
         # that raises the error is passed to assertRaises!
-        self.assertRaises(KeyError,
-                          lambda: response.context['form'].errors,
-                          )
+        self.assertRaises(
+            KeyError,
+            lambda: response.context["form"].errors,
+        )
 
-        # did we get plausible data back?
-        su = User.objects.first()
+        # Did we get plausible data back?
+        su = User.objects.get(username=self.superuser)
         self.assertIsNotNone(su.mitglied.festnetz)
 
-        # log back in again; now the error should be gone
+        # Log back in again
         cl2, response = self.login_user(self.superuser)
-        self.assertEqual(response.status_code, 200)
-
-        # we should end up on home:
-        self.assertEqual("/", response.request['PATH_INFO'])
-
-        # there must be no message on the home page:
-        pp(response.context['messages'])
-        pp(list(response.context['messages']))
-        self.assertListEqual([], list(response.context['messages']))
-        # self.assertRaises(KeyError,
-        #                   lambda: response.context['messages'])
+        # We should end up on home
+        self.assertEqual("/", response.request["PATH_INFO"])
+        # There must be no message on the home page:
+        self.assertListEqual([], list(response.context["messages"]))
 
     def test_change_password(self):
         cl, response = self.login_user(self.superuser)
-        self.assertEqual(response.status_code, 200)
+        self.assertNotIn("login", response.request["PATH_INFO"])
 
+        # Initiate password change
         response = cl.get("/password/change/")
-
-        form = response.context['form']
-
+        form = response.context["form"]
         data = form.initial
-        data['pw1'] = self.plainpassword + self.plainpassword
-        data['pw2'] = data['pw1']
+        data["pw1"] = self.plainpassword + self.plainpassword
+        data["pw2"] = data["pw1"]
 
-        response = cl.post("/password/change/",
-                           data,
-                           follow=True)
-
+        # Actually change password
+        response = cl.post("/password/change/", data, follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual("/", response.request['PATH_INFO'])
+        self.assertEqual("/", response.request["PATH_INFO"])
+        self.assertContains(response, "Ihr Passwort wurde erfolgreich")
 
-        self.assertContains(response,
-                            "Ihr Passwort wurde erfolgreich")
-
-        cl, response = self.login_user(self.superuser,
-                                       self.plainpassword + self.plainpassword)
-        self.assertEqual(response.status_code, 200)
+        # Test login with new password
+        cl, response = self.login_user(
+            self.superuser, self.plainpassword + self.plainpassword
+        )
+        self.assertNotIn("login", response.request["PATH_INFO"])
