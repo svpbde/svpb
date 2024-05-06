3bcf749fe5b7dc4e952de68898fa5b0ef040fe0d
Dmitrij Dreiling <d.dimka89@gmail.com>
2024-05-06 21:48:09 +0200

    Update to django 4.2 (dirty)
    Dirty because incompatible with django-password-reset 2.0 and therefore links to an updated dev version on github. Better to drop django-password-reset and use the built-in password manager functions.

--- svpb/urls.py
+++ svpb/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
@@ -18,69 +18,69 @@ admin.autodiscover()
 
 urlpatterns = [
 
-    url(r'^$',
+    re_path(r'^$',
         TemplateView.as_view(template_name="main.html"),
         name="main",
         ),
 
-    url(r'^home/$',
+    re_path(r'^home/$',
         TemplateView.as_view(template_name="main.html"),
         name="mainHome",
         ),
 
-    url(r'^keinVorstand/$',
+    re_path(r'^keinVorstand/$',
         TemplateView.as_view(template_name='keinVorstand.html'),
         name="keinVorstand",
         ),
 
-    url(r'^arbeitsplan/', include('arbeitsplan.urls')),
+    re_path(r'^arbeitsplan/', include('arbeitsplan.urls')),
 
-    url(r'^accounts/', include('mitglieder.urls')),
+    re_path(r'^accounts/', include('mitglieder.urls')),
 
-    url(r'^boote/', include('boote.urls')),
+    re_path(r'^boote/', include('boote.urls')),
 
-    url(r'^about$',
+    re_path(r'^about$',
         TemplateView.as_view(template_name="about.html"),
         name="about",
         ),
 
-    url(r'^dsgvo$',
+    re_path(r'^dsgvo$',
         TemplateView.as_view(template_name="datenschutzerklaerung.html"),
         name="about",
         ),
 
-    # url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin/', admin.site.urls),
+    # re_path(r'^admin/', include(admin.site.urls)),
+    re_path(r'^admin/', admin.site.urls),
 
-    url(r'^login/',
+    re_path(r'^login/',
         svpb.views.SvpbLogin.as_view(),
         name="login"),
 
-    url(r'^logout/', svpb.views.logout_view),
+    re_path(r'^logout/', svpb.views.logout_view),
 
-    url(r'^password/change/$',
+    re_path(r'^password/change/$',
         active_and_login_required(mitglieder.views.PasswordChange.as_view()),
         name='password_change',
         ),
 
     # media for manual intergration:
-    url(r'^manual/',
+    re_path(r'^manual/',
         active_and_login_required(arbeitsplan.views.MediaChecks.as_view()),
         name="MediaCheck",
         ),
 
     # Impersonation of other users:
-    url(r'^impersonate/liste/$',
+    re_path(r'^impersonate/liste/$',
         active_and_login_required(mitglieder.views.ImpersonateListe.as_view()),
         name="arbeitsplan-impersonateListe",
         ),
-    url(r'^impersonate/', include('impersonate.urls')),
+    re_path(r'^impersonate/', include('impersonate.urls')),
 
     # password reset; compare http://django-password-reset.readthedocs.org/en/latest/quickstart.html
-    url(r'^reset/', include('password_reset.urls')),
+    re_path(r'^reset/', include('password_reset.urls')),
 
     # django select2, see: https://github.com/codingjoe/django-select2
-    url(r'^select2/', include('django_select2.urls')),
+    re_path(r'^select2/', include('django_select2.urls')),
 
     ]
 
