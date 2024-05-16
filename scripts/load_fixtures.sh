3340ba5f7729f47dd27748524ae309794d19f3c7
Tim Hetkämper <transistortim@hetkaemper.it>
2024-05-16 22:20:02 +0200

    Update schema privileges for postgresql >= 15

--- scripts/load_fixtures.sh
+++ scripts/load_fixtures.sh
@@ -3,6 +3,7 @@ cd ..
 sudo -u postgres dropdb --force --interactive svpbdata
 sudo -u postgres createdb svpbdata
 sudo -u postgres psql -c "grant all privileges on database svpbdata to svpbdb;"
+sudo -u postgres psql -c "\connect svpbdata" -c "grant usage, create on schema public to svpbdb;"
 # Init database with fixtures
 python manage.py migrate
 python manage.py loaddata mitglieder/fixtures/*.json
