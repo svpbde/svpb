6322cb00c6d359e75d434a4dbfdfc73c30f38d66
Tim Hetkämper <transistortim@hetkaemper.it>
2024-12-09 20:25:25 +0100

    Add command to reset database at year end (#44)
    Fixes #31

---
+++ arbeitsplan/management/commands/reset_database_year_end.py
@@ -0,0 +1,58 @@
+"""Reset database at year end to prepare new year."""
+from django.contrib.sessions.models import Session
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from django.utils import translation
+from django.conf import settings
+
+from arbeitsplan.models import Leistung, Zuteilung, Meldung
+
+
+class Command(BaseCommand):
+    """Reset database at year end to prepare new year.
+
+    This command resets the database at year end by deleting all entries of
+      - Leistung
+      - Zuteilung
+      - Meldung
+      - Session (to ensure everyone is logged out)
+      - Mails (deletion is not necessary, but keeps database clean)
+
+    Tasks are kept deliberately, as they do not differ much over the years.
+    """
+    help = "Reset database at year end to prepare new year."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--force", action="store_true", help="Do not ask interactive questions"
+        )
+
+    def handle(self, *args, **options):
+        # Set the locale right, to get the dates represented correctly
+        translation.activate(settings.LANGUAGE_CODE)
+
+        if not options["force"]:
+            choice = input("Are you sure you want to delete ALL entries? [y/N]: ")
+            if choice != "y":
+                return
+
+        # Delete entries related to working hours
+        num_deletions, per_object = Leistung.objects.all().delete()
+        print(f"Leistung: Deleted {num_deletions} objects, consisting of {per_object}")
+        # Note that deletions via the QuerySet API do not call the model's delete()
+        # method, so luckily the custom code modifying zuteilungBenachrichtigungNoetig
+        # is not triggered.
+        # See https://docs.djangoproject.com/en/4.2/ref/models/querysets/#delete
+        num_deletions, per_object = Zuteilung.objects.all().delete()
+        print(f"Zuteilung: Deleted {num_deletions} objects, consisting of {per_object}")
+        num_deletions, per_object = Meldung.objects.all().delete()
+        print(f"Meldung: Deleted {num_deletions} objects, consisting of {per_object}")
+
+        # Delete sessions to ensure everyone is logged out
+        num_deletions, per_object = Session.objects.all().delete()
+        print(f"Session: Deleted {num_deletions} objects, consisting of {per_object}")
+
+        # Delete mails
+        call_command("cleanup_mail", days=0, delete_attachments=True)
+
+        translation.deactivate()
