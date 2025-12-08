dfa8be46dafdfac4a79863e74218b23cf159c352
Tim Hetkämper <transistortim@hetkaemper.it>
2025-12-08 21:33:30 +0100

    Improve code style

--- arbeitsplan/management/commands/reservationEmails.py
+++ arbeitsplan/management/commands/reservationEmails.py
@@ -1,85 +1,91 @@
-"""
-Define a command that should be run from a crontab.
-Check offene oder Rueckfragen Leistungsmeldungen. 
-Send an email to the person in charge. 
-"""
+"""Send boat reservation and boat issue mails."""
 
-from django.core.management.base import BaseCommand, CommandError
-from django.core.management import call_command
-from django.utils import translation
 from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
 from django.template import loader, Context
-
+from django.utils import translation
 from post_office import mail
 
 import boote.models as models
 
-import datetime
-import pprint
 
 class Command(BaseCommand):
-    """Go through all reservations 
-    and send out 
-    """
-    help = "Send out Reservations."
-    
+    """Send boat reservation and boat issue mails."""
+
+    help = """Send boat reservation and boat issue mails."""
 
     def handle(self, *args, **options):
-        # set the locale right, to get the dates represented correctly
+        # Set the locale right, to get the dates represented correctly
         translation.activate(settings.LANGUAGE_CODE)
 
-
         # EMAIL RESERVATION
-        t = loader.get_template("boote/email_booking.html")        
-        # get records        
-        for booking in models.Booking.objects.filter(notified = False).order_by('-date'):
+        t = loader.get_template("boote/email_booking.html")
+        # Get records
+        for booking in models.Booking.objects.filter(notified=False).order_by("-date"):
             try:
-                c = Context({ 'booking': booking})
+                c = Context({"booking": booking})
                 payload = t.render(c)
-                sbj = '[SVPB] Reservierung (' + booking.date.strftime('%d.%m.%Y') + ') - ' + booking.boat.type.name + " \"" + booking.boat.name + "\"" 
-                self.stdout.write('From: ' +  settings.DEFAULT_FROM_EMAIL)
-                self.stdout.write('To: ' + booking.user.email)
-                self.stdout.write('Subject: ' + sbj)
-                self.stdout.write('Content:\n\r' + payload)
-            
+                sbj = (
+                    "[SVPB] Reservierung ("
+                    + booking.date.strftime("%d.%m.%Y")
+                    + ") - "
+                    + booking.boat.type.name
+                    + ' "'
+                    + booking.boat.name
+                    + '"'
+                )
+                self.stdout.write("From: " + settings.DEFAULT_FROM_EMAIL)
+                self.stdout.write("To: " + booking.user.email)
+                self.stdout.write("Subject: " + sbj)
+                self.stdout.write("Content:\n\r" + payload)
+
                 mail.send(
-                    [booking.user.email], 
+                    [booking.user.email],
                     settings.DEFAULT_FROM_EMAIL,
-                    subject=sbj,                  
+                    subject=sbj,
                     html_message=payload,
                 )
-            
+
                 booking.notified = True
                 booking.save()
             except:
-                print("Unexpected error ")     
-            
+                print("Unexpected error ")
+
         # EMAIL ISSUES
-        t = loader.get_template("boote/email_issue.html")        
-        # get records        
-        for issue in models.BoatIssue.objects.filter(notified = False):
+        t = loader.get_template("boote/email_issue.html")
+        # Get records
+        for issue in models.BoatIssue.objects.filter(notified=False):
             try:
-                c = Context({ 'issue': issue})
+                c = Context({"issue": issue})
                 payload = t.render(c)
-                recipients = settings.EMAIL_NOTIFICATION_BOATS + [issue.reported_by.email]
-                sbj = '[SVPB] Schadensmeldung - ' + issue.boat.type.name + " \"" + issue.boat.name + "\"" 
-                self.stdout.write('From: ' +  settings.DEFAULT_FROM_EMAIL)
-                self.stdout.write(f'To: {recipients}')
-                self.stdout.write('Subject: ' + sbj)
-                self.stdout.write('Content:\n\r' + payload)
-                
+                recipients = settings.EMAIL_NOTIFICATION_BOATS + [
+                    issue.reported_by.email
+                ]
+                sbj = (
+                    "[SVPB] Schadensmeldung - "
+                    + issue.boat.type.name
+                    + ' "'
+                    + issue.boat.name
+                    + '"'
+                )
+                self.stdout.write("From: " + settings.DEFAULT_FROM_EMAIL)
+                self.stdout.write(f"To: {recipients}")
+                self.stdout.write("Subject: " + sbj)
+                self.stdout.write("Content:\n\r" + payload)
+
                 mail.send(
-                            recipients,
-                            settings.DEFAULT_FROM_EMAIL,
-                            subject=sbj,                  
-                            html_message=payload,
-                            )
-            
+                    recipients,
+                    settings.DEFAULT_FROM_EMAIL,
+                    subject=sbj,
+                    html_message=payload,
+                )
+
                 issue.notified = True
                 issue.save()
             except:
-                print("Unexpected error ")  
+                print("Unexpected error ")
 
-        call_command('send_queued_mail')
+        call_command("send_queued_mail")
 
         translation.deactivate()
