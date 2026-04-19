d4f82890a46c13d68ee24f8d58a41aa0d138d745
Tim Hetkämper <transistortim@hetkaemper.it>
2026-04-19 12:55:25 +0200

    Store database dumps in backup directory

--- scripts/dump_database.sh
+++ scripts/dump_database.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 timestamp=`date +%Y-%m-%dT%H%M%S`
 cd ..
-python manage.py dumpdata --exclude=auth.permission --exclude=contenttypes --exclude=sessions -o "${timestamp}_database_dump.json"
+python manage.py dumpdata --exclude=auth.permission --exclude=contenttypes --exclude=sessions -o "backup/${timestamp}_database_dump.json"
